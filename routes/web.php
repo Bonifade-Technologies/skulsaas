80271972ee28ef5993e8bcd8a08621ac58b3f69e
oyenet1 <bowofadeoyerinde@gmail.com>
2023-07-10 22:30:05 +0100

    terms and session done

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/test', function () {
+    return \App\Models\Year::with('terms')->get()->toArray();
+});
 
 Route::get('/home', function () {
     return view('layouts.app');
