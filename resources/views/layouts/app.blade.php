6acbfa604649088d07537fb19217e073a312d90c
oyenet1 <bowofadeoyerinde@gmail.com>
2023-07-10 21:28:18 +0100

    ok done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -382,10 +382,10 @@
         id="dropdown-2">
         <div class="px-4 py-3" role="none">
          <p class="text-sm text-gray-900 dark:text-white" role="none">
-          Neil Sims
+          {{auth()->user()->name}}
          </p>
          <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
-          neil.sims@flowbite.com
+          {{auth()->user()->email}}
          </p>
         </div>
         <ul class="py-1" role="none">
