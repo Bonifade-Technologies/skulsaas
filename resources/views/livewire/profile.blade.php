c08df3a47e50fff601accf3a7d1e9f528d7633d2
oyenet1 <bowofadeoyerinde@gmail.com>
2023-07-16 04:36:01 +0100

    profile done

--- resources/views/livewire/profile.blade.php
+++ resources/views/livewire/profile.blade.php
@@ -1,9 +1,9 @@
 <div class="w-full">
     <div class="grid grid-cols-1 space-y-4">
         <div class="space-y-4">
-            <x-bread-crumb :inp="array('name'=> 'Settings', 'url'=> 'admin/settings')" />
+            <x-bread-crumb :inp="array('name'=> 'Profile', 'url'=> 'profile')" />
             <div class="flex items-center space-x-4">
-                <x-title title="School Settings" />
+                <x-title title="Profile" />
             </div>
         </div>
         <div class="col-span-full xl:col-auto">
