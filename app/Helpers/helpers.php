3e378a8dc4ca5bcd570fd3fe2840faa545f9d372
Bowofade Oyerinde <bowofadeoyerinde@gmail.com>
2023-10-08 22:26:27 +0100

    logo and name

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -6,82 +6,82 @@ use PhpParser\Node\Expr\Cast\Object_;
 
 function currentUser()
 {
- return auth()->user();
+    return auth()->user();
 }
 
 function currentSession(): object|null
 {
- $term = \App\Models\Year::where('is_current', true)->first();
- return $term ?? null;
+    $term = \App\Models\Year::where('is_current', true)->first();
+    return $term ?? null;
 }
 function currentTerm(): object|null
 {
- $terms = \App\Models\Term::get();
- return $terms->count() ? \App\Models\Term::with(['year:id,session_name'])->orderByDesc('id')->first() : null;
+    $terms = \App\Models\Term::get();
+    return $terms->count() ? \App\Models\Term::with(['year:id,session_name'])->orderByDesc('id')->first() : null;
 }
 
 function getCountries()
 {
- $countries = Http::get('https://restcountries.com/v3.1/all?fields=name,idd,flags,currencies,capital');
- return $countries;
+    $countries = Http::get('https://restcountries.com/v3.1/all?fields=name,idd,flags,currencies,capital');
+    return $countries;
 }
 
 function settings()
 {
- return \App\Models\Setting::with('media')->first();
- // return \App\Models\Setting::with('media')->where('tenant_id', auth()->user()->id)->first();
+    //  return \App\Models\Setting::with('media')->first();
+    return \App\Models\Setting::with('media')->where('tenant_id', auth()->user()->current_tenant_id)->first();
 }
 function redirectback()
 {
- return redirect()->back();
+    return redirect()->back();
 }
 
 
 function currentUserPermissions(): array|null
 {
- $permissions = \App\Models\Permission::
-  select('name')
-  ->whereRelation('users', 'id', Auth::id())
-  ->orWhereRelation('roles', 'id', auth()->user()->current_role_id)
-  ->pluck('name')
-  ->toArray();
- return $permissions;
+    $permissions = \App\Models\Permission::
+        select('name')
+        ->whereRelation('users', 'id', Auth::id())
+        ->orWhereRelation('roles', 'id', auth()->user()->current_role_id)
+        ->pluck('name')
+        ->toArray();
+    return $permissions;
 }
 
 
 function statusColor($status)
 {
- // $color = "bg-gray-500 text-gray-50";
- switch ($status) {
-  case 'active':
-   $color = "border-green-500 text-green-500";
-   break;
-  case 'created':
-   $color = "border-green-300 text-green-300";
-   break;
-  case 'accepted':
-   $color = "border-green-500 text-green-500";
-   break;
-  case 'onLeave':
-   $color = "border-orange-400 text-orange-400";
-   break;
-  case 'credit':
-   $color = "border-green-500 text-wgreen-500";
-   break;
-  case 'debit':
-   $color = "border-red-500 text-red-500";
-   break;
-  case 'expired':
-   $color = "border-red-500 text-red-500";
-   break;
-  case 'graduated':
-   $color = "border-red-900 text-red-900";
-   break;
-  default:
-   $color = "bg-gray-500 text-gray-50";
-   break;
- }
+    // $color = "bg-gray-500 text-gray-50";
+    switch ($status) {
+        case 'active':
+            $color = "border-green-500 text-green-500";
+            break;
+        case 'created':
+            $color = "border-green-300 text-green-300";
+            break;
+        case 'accepted':
+            $color = "border-green-500 text-green-500";
+            break;
+        case 'onLeave':
+            $color = "border-orange-400 text-orange-400";
+            break;
+        case 'credit':
+            $color = "border-green-500 text-wgreen-500";
+            break;
+        case 'debit':
+            $color = "border-red-500 text-red-500";
+            break;
+        case 'expired':
+            $color = "border-red-500 text-red-500";
+            break;
+        case 'graduated':
+            $color = "border-red-900 text-red-900";
+            break;
+        default:
+            $color = "bg-gray-500 text-gray-50";
+            break;
+    }
 
- return $color;
+    return $color;
 }
 ?>
--- resources/views/livewire/settings.blade.php
+++ resources/views/livewire/settings.blade.php
@@ -1,6 +1,6 @@
 <div class="grid w-full grid-cols-1 space-y-4">
     <div class="space-y-4">
-        <x-bread-crumb :inp="array('name'=> 'Settings', 'url'=> 'admin/settings')" />
+        <x-bread-crumb :inp="['name' => 'Settings', 'url' => 'admin/settings']" />
         <div class="flex items-center space-x-4">
             <x-title title="School Settings" />
         </div>
@@ -11,44 +11,45 @@
             class="p-4 mb-4 bg-white border border-gray-200 rounded-lg shadow-sm 2xl:col-span-2 dark:border-gray-700 sm:p-6 dark:bg-gray-800">
             <div class="items-center sm:flex xl:block 2xl:flex sm:space-x-4 xl:space-x-0 2xl:space-x-4">
                 <input type="file" wire:model.defer="school_logo" id="school_logo" class="hidden">
-                <img src="{{ $school_logo ? $school_logo->temporaryUrl(): $con?->getFirstMediaUrl('setting') }}"
-                    alt="{{ $con?->school_logo }}" class="mt-2 h-20 w-20 rounded-full border border-gray-200 object-cover shadow lg:h-24 lg:w-24 xl:h-28
-        xl:w-28">
+                <img src="{{ settings()->getFirstMediaUrl('setting') }}" alt="{{ $con?->school_logo }}"
+                    class="object-cover w-20 h-20 mt-2 border border-gray-200 rounded-full shadow lg:h-24 lg:w-24 xl:h-28 xl:w-28">
                 <div>
                     <h3 class="mb-1 text-xl font-bold text-gray-900 dark:text-white">School Logo
                     </h3>
                     <div class="mb-4 text-sm text-gray-500 dark:text-gray-400">
                         @if ($errors->any())
-                        @error('school_logo')
-                        <span class="mb-2 text-red-600">{{ $message }}</span>
-                        @enderror
+                            @error('school_logo')
+                                <span class="mb-2 text-red-600">{{ $message }}</span>
+                            @enderror
                         @else
-                        <span> JPG, GIF or PNG. Max size of 500K</span>
+                            <span> JPG, GIF or PNG. Max size of 500K</span>
                         @endif
                     </div>
                     <div class="flex items-center space-x-4">
                         @if ($school_logo)
-                        <button wire:click="changeSchoolLogo()"
-                            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                            <svg class="w-4 h-4 mr-2 -ml-1" fill="currentColor" viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M5.5 13a3.5 3.5 0 01-.369-6.98 4 4 0 117.753-1.977A4.5 4.5 0 1113.5 13H11V9.413l1.293 1.293a1 1 0 001.414-1.414l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13H5.5z">
-                                </path>
-                                <path d="M9 13h2v5a1 1 0 11-2 0v-5z"></path>
-                            </svg>
-                            Upload logo
-                        </button>
+                            <button wire:click="changeSchoolLogo()"
+                                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                                <svg class="w-4 h-4 mr-2 -ml-1" fill="currentColor" viewBox="0 0 20 20"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M5.5 13a3.5 3.5 0 01-.369-6.98 4 4 0 117.753-1.977A4.5 4.5 0 1113.5 13H11V9.413l1.293 1.293a1 1 0 001.414-1.414l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13H5.5z">
+                                    </path>
+                                    <path d="M9 13h2v5a1 1 0 11-2 0v-5z"></path>
+                                </svg>
+                                Upload logo
+                            </button>
                         @else
-                        <label for="school_logo"
-                            class="px-4 py-2 text-sm rounded-full shadow bg-gray-200 text-black cursor-pointer">{{ $con?->school_logo ? 'Change logo': 'Choose
-                            Logo' }}</label>
+                            <label for="school_logo"
+                                class="px-4 py-2 text-sm text-black bg-gray-200 rounded-full shadow cursor-pointer">{{ $con?->school_logo
+                                    ? 'Change logo'
+                                    : 'Choose
+                                                            Logo' }}</label>
                         @endif
                         @if ($school_logo)
-                        <button wire:click="removeLogo()"
-                            class="py-2 px-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-red-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-900 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-                            Delete
-                        </button>
+                            <button wire:click="removeLogo()"
+                                class="px-3 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg focus:outline-none hover:bg-gray-100 hover:text-red-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-900 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                                Delete
+                            </button>
                         @endif
                     </div>
                 </div>
@@ -59,25 +60,25 @@
             <h3 class="mb-4 text-xl font-semibold dark:text-white">School information</h3>
             <div class="w-full overflow-x-auto">
                 <form wire:submit.prevent="updateSchoolInfo" class="w-full">
-                    <table class="w-full oveflow-x-auto border-collapse">
-                        <tr class="flex border divide-x flex-col md:flex-row lg:flex-none w-full min-w-full">
-                            {{-- <td class="border p-2">
+                    <table class="w-full border-collapse oveflow-x-auto">
+                        <tr class="flex flex-col w-full min-w-full border divide-x md:flex-row lg:flex-none">
+                            {{-- <td class="p-2 border">
                                     <x-input.single-select :list="['first', 'second', 'third']" label="name" name="name"
-                                        class=" rounded" />
+                                        class="rounded " />
                                 </td> --}}
-                            <td class=" p-2">
-                                <x-input.text label="Country" name="school_country" class=" rounded" />
+                            <td class="p-2 ">
+                                <x-input.text label="Country" name="school_country" class="rounded " />
                             </td>
-                            <td class=" p-2">
-                                <x-input.text label="School Name" name="school_name" class=" rounded" />
+                            <td class="p-2 ">
+                                <x-input.text label="School Name" name="school_name" class="rounded " />
                             </td>
-                            <td class=" p-2">
-                                <x-input.text label="School Email" name="school_email" class=" rounded" />
+                            <td class="p-2 ">
+                                <x-input.text label="School Email" name="school_email" class="rounded " />
                             </td>
-                            <td class=" p-2">
-                                <x-input.text label="Telephone" name="school_phone" class=" rounded" />
+                            <td class="p-2 ">
+                                <x-input.text label="Telephone" name="school_phone" class="rounded " />
                             </td>
-                            <td class=" p-2 align-middle">
+                            <td class="p-2 align-middle ">
                                 <button wire:loading.attr="disabled"
                                     class="text-white bg-primary mt-6 disabled:bg-gray-700 whitespace-nowrap capitalize disabled:text-gray-500 hover:bg-primary-600 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                                     type="submit">update</button>
@@ -91,8 +92,8 @@
     <div class="col-span-full xl:col-auto">
         <div
             class="p-4 mb-4 space-y-4 bg-white border border-gray-200 rounded-lg shadow-sm 2xl:col-span-2 dark:border-gray-700 sm:p-6 dark:bg-gray-800">
-            <h3 class=" text-xl font-semibold dark:text-white">School Categories</h3>
-            <form wire:submit.prevent="@if(!$update)  saveSchoolName @else updateSchoolType @endif">
+            <h3 class="text-xl font-semibold  dark:text-white">School Categories</h3>
+            <form wire:submit.prevent="@if (!$update) saveSchoolName @else updateSchoolType @endif">
                 <div class="flex items-end justify-start gap-4">
                     <div class="max-w-[300px]">
                         <x-input.text label="Category name" name="name" />
@@ -103,13 +104,13 @@
             <x-tooltip pers="to" content="Click to edit" />
             <div class="flex flex-wrap gap-4">
                 @forelse (\App\Models\SchoolType::select('id', 'name')->get() as $key => $item)
-                <p id="{{ $key }}" wire:click="editType({{ $item->id }})" data-tooltip-target="to"
-                    data-tooltip-placement="top"
-                    class="min-w-[150px] text-center justify-center max-w-[200px] capitalize cursor-pointer tt bg-gray-100 px-3 py-1 rounded shadow">
-                    <span>{{ $item->name }}</span>
-                </p>
+                    <p id="{{ $key }}" wire:click="editType({{ $item->id }})" data-tooltip-target="to"
+                        data-tooltip-placement="top"
+                        class="min-w-[150px] text-center justify-center max-w-[200px] capitalize cursor-pointer tt bg-gray-100 px-3 py-1 rounded shadow">
+                        <span>{{ $item->name }}</span>
+                    </p>
                 @empty
-                <p>No school categories yet add one</p>
+                    <p>No school categories yet add one</p>
                 @endforelse
             </div>
         </div>
